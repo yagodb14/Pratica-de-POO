fec01bc816136cdc54c6e221990bb06d208cb28b
yagodb14 <yagodanb14@gmail.com>
2026-09-14 14:19:25 -0300

    Trabalho de nota

---
+++ livro/src/main/java/br/com/senac/livro/Livro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.senac.livro;
+
+/**
+ *
+ * @author yago62977756
+ */
+public class Livro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
